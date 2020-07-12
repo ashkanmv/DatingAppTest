da440dbb93492980997dc1d0a82f51507351bb06
ashkan <ashkanmv@gmail.com>
2020-07-12 04:36:21 +0430

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>option):base(option) { }
+
+
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
